4d2f93e68da831701ae61d9c445271358b200086
tkfr <weixiao@tkfr.fr>
2014-08-05 14:23:50 +0200

    test1

---
+++ modules/blocklanguages/translations/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{blocklanguages}prestashop>blocklanguages_d5988791c07fedc0e2fc77683b4e61f6'] = 'Bloc langues';
+$_MODULE['<{blocklanguages}prestashop>blocklanguages_5bc2cbadb5e09b5ef9b9d1724072c4f9'] = 'Ajoute un bloc permettant à vos clients de sélectionner la langue de votre boutique.';
+
+
+return $_MODULE;
---
+++ modules/statsequipment/translations/fr.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{statsequipment}prestashop>statsequipment_247270d410e2b9de01814b82111becda'] = 'Navigateurs web et systèmes d\'exploitation';
+$_MODULE['<{statsequipment}prestashop>statsequipment_2876718a648dea03aaafd4b5a63b1efe'] = 'Ajoute un onglet contenant un graphique des navigateurs web et systèmes d\'exploitation utilisé, dans le tableau de bord des statistiques.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_6602bbeb2956c035fb4cb5e844a4861b'] = 'Guide';
+$_MODULE['<{statsequipment}prestashop>statsequipment_854c8e126f839cc861cde822b641230e'] = 'S\'assurer que votre site web est accessible au plus grand nombre de personnes possible';
+$_MODULE['<{statsequipment}prestashop>statsequipment_00607ff6337f1ee20badf91b3e5042b3'] = 'Lorsque l\'on gère des sites web, il est important de garder une trace des logiciels utilisés par les visiteurs afin de s\'assurer que le site s\'affiche de la même façon pour tout le monde. PrestaShop a été construit de manière à être compatible avec la plupart des navigateurs Web et systèmes d\'exploitation récents. Toutefois, dans le cas où vous auriez ajouté des fonctionnalités avancées à votre site Web ou même modifié le code de base PrestaShop, ces ajouts pourraient ne pas être accessibles par tous. C\'est pourquoi il est bon de contrôler le pourcentage de visiteurs pour chaque système avant de songer à ajouter ce type de fonctionnalités trop spécifiques.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_11db1362a88c5e3e74c8f699c14d6798'] = 'Indique la popularité de chaque navigateur, en pourcentage d\'utilisation sur la base de vos clients.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_998e4c5c80f27dec552e99dfed34889a'] = 'Export CSV';
+$_MODULE['<{statsequipment}prestashop>statsequipment_90c58bfe4872fc9ca7bf6a181c3e5edd'] = 'Indique le pourcentage de chaque système d\'exploitation utilisé par les clients.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_bb38096ab39160dc20d44f3ea6b44507'] = 'Extensions';
+$_MODULE['<{statsequipment}prestashop>statsequipment_9ffafc9e090c8e1c06f928ef2817efd6'] = 'Navigateurs utilisés';
+$_MODULE['<{statsequipment}prestashop>statsequipment_0241b7aaaa5f76afd585bb6cdae314d1'] = 'Systèmes d\'exploitation utilisés';
+
+
+return $_MODULE;
